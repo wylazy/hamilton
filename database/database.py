8de7298a96ecf3ffe5e51b502c5a626ea0d118dd
wylazy <ipjmcp@gmail.com>
2013-08-27 00:49:06 +0800

    设置Python文件编码

--- database/database.py
+++ database/database.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
